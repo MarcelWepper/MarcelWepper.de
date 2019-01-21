0eb011ca8d2da99a46e3461b48164336b5080ba3
Marcel-Rene Wepper <39297624+Envolpes@users.noreply.github.com>
2019-01-21 18:50:16 +0100

    FontAwesome + Icon

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,6 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './Home.css';
 import  Picture from'./Pics/code_background.jpg';
 import  Profile from'./Pics/me.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWineBottle } from '@fortawesome/free-solid-svg-icons'
+
 
 export default class Home extends React.Component {
 	render(){
@@ -23,6 +26,19 @@ export default class Home extends React.Component {
 							</Row>
 							<Row>
 								<Col><h3> HTML/CSS | Mobile Design | CMS | Project-Management | DB-Design</h3></Col>
+							</Row>
+
+							<Row>
+								<div className="SocialLinks">
+									<Col>{/* LinkedIn Icon */}
+									<a href="https://www.linkedin.com/in/marcel-rené-wepper-a25421169/" rel="noopener noreferrer" target="_blank">
+										<i className ="fa fa-linkedin-square"  aria-hidden="true" />
+									</a></Col>
+									<Col>{/* Weppes Icon */}
+										<a href="http://weppes.de" rel="noopener noreferrer" target="_blank">
+											<i className ="fas fa-wine-bottle " color="white"  aria-hidden="true" />
+										</a></Col>
+								</div>
 							</Row>
 						</div>
 					</Container>
